0805492adb1edf32470606cde3d2fc6d397d93c0
Julio Cesar Fernandez <j.c.jf285@gmail.com>
2025-11-15 18:12:35 -0800

    experimenting with navigation

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,18 @@
 'use client'; // For state
 import { useState } from 'react';
 import { Button } from 'react-bootstrap';
+import { useRouter } from 'next/navigation';
+
 
 export default function Home() {
   const [count, setCount] = useState(0);
+  const router = useRouter();
+
   return (
     <div className="container">
       <h1>Counter: {count}</h1>
       <Button variant="success" onClick={() => setCount(count + 1)}>Increment</Button>
+      <Button variant="success" onClick={async() => router.push("/signUp")}>signUp</Button>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
-'use client'; // For state
-import { useState } from 'react';
-import { Button } from 'react-bootstrap';
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  const [count, setCount] = useState(0);
-  return (
-    <div className="container">
-      <h1>Counter: {count}</h1>
-      <Button variant="success" onClick={() => setCount(count + 1)}>Increment</Button>
-    </div>
-  );
+  redirect("/login");
 }
