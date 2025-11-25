17dd93d74150e89c4b438365a98d0ddb2d542bbf
Julio Cesar Fernandez <j.c.jf285@gmail.com>
2025-11-25 13:26:23 -0800

    fixed inability to scoll issue

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,23 @@
+'use client';
+
 import { FaHome, FaCompass, FaPlusSquare, FaBell, FaUser, FaEnvelope, FaSearch } from "react-icons/fa";
 import styles from '../app/styles/navbar.module.css';
+import { useRouter } from 'next/navigation';
 
 export default function Navbar() {
+  const router = useRouter();
+
+  function doNothing() {
+    //  for now do nothing
+    return;
+  } 
+
   return (
     <nav className={styles.navbar}>
-      <FaHome className={styles.icon} />
-      <FaUser className={styles.icon} />
-      <FaCompass className={styles.icon} />
-      <FaPlusSquare className={styles.icon} />
+      <FaHome className={styles.icon} onClick={async () => router.push("/home")}/>
+      <FaUser className={styles.icon} onClick={async () => router.push("/myProfile")} />
+      <FaCompass className={styles.icon} onClick={async () => router.push("/explore")}/>
+      <FaPlusSquare className={styles.icon} onClick={doNothing}/>
       <FaBell className={styles.icon} />
     </nav>
   );
