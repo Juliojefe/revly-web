fd0129b138ad045fd054b474980efa1936437300
Julio Cesar Fernandez <j.c.jf285@gmail.com>
2025-11-25 18:22:52 -0800

    navbar finalized

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,23 +2,38 @@
 
 import { FaHome, FaCompass, FaPlusSquare, FaBell, FaUser, FaEnvelope, FaSearch } from "react-icons/fa";
 import styles from '../app/styles/navbar.module.css';
-import { useRouter } from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation';
 
 export default function Navbar() {
   const router = useRouter();
+  const pathname = usePathname();
 
   function doNothing() {
-    //  for now do nothing
-    return;
-  } 
+    return; //  for now do nothing
+  }
 
   return (
     <nav className={styles.navbar}>
-      <FaHome className={styles.icon} onClick={async () => router.push("/home")}/>
-      <FaUser className={styles.icon} onClick={async () => router.push("/myProfile")} />
-      <FaCompass className={styles.icon} onClick={async () => router.push("/explore")}/>
-      <FaPlusSquare className={styles.icon} onClick={doNothing}/>
-      <FaBell className={styles.icon} onClick={doNothing}/>
+      <FaHome
+        className={`${styles.icon} ${pathname === '/home' ? styles.active : ''}`}
+        onClick={async () => router.push("/home")}
+      />
+      <FaUser
+        className={`${styles.icon} ${pathname === '/myProfile' ? styles.active : ''}`}
+        onClick={async () => router.push("/myProfile")}
+      />
+      <FaCompass
+        className={`${styles.icon} ${pathname === '/explore' ? styles.active : ''}`}
+        onClick={async () => router.push("/explore")}
+      />
+      <FaPlusSquare
+        className={styles.icon}
+        onClick={doNothing}
+      />
+      <FaBell
+        className={styles.icon}
+        onClick={doNothing}
+      />
     </nav>
   );
 }
