3fae4f1f406c2d4f265abb756304a747234fe537
Julio Cesar Fernandez <j.c.jf285@gmail.com>
2025-11-27 12:50:12 -0800

    PostList loops reusable component

--- app/(app)/explore/page.tsx
+++ app/(app)/explore/page.tsx
@@ -6,13 +6,12 @@ import { useUser } from '@/context/UserContext';
 import { PostType } from '@/types/post';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import Post from '@/components/Post';
+import PostList from '@/components/PostList';
 
 
 export default function explore() {
   const router = useRouter();
   const { user, logout } = useUser();
-  // const testPostIds = [1, 47, 48];    //  text only, one image, two images
   const testPostIds = [1, 2, 3, 67, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 34, 35, 36, 100, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53];
   const [postData, setPostData] = useState<PostType[]>([]);
 
@@ -41,9 +40,7 @@ export default function explore() {
     <div className={styles.container}>
       <div className={styles.postContentContainer}>
         <h2>My Explore Page</h2>
-        {postData.map((post) => (
-          <Post key={post.postId} postData={post} />
-        ))}
+          <PostList postDataArray={postData} />
       </div>
     </div>
   );
---
+++ components/PostList.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import React, { useState } from "react";
+import { PostType } from '@/types/post';
+import Post from '@/components/Post';
+
+interface PostListProps {
+  postDataArray?: PostType[];
+}
+
+export default function PostList({ postDataArray = [] }: PostListProps) {
+  return (
+    postDataArray.map((post) => (
+      <Post key={post.postId} postData={post} />
+    ))
+  );
+}
