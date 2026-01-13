cb2c33fc2d4845ba9cf4fa83c5630e9dfeb750b2
Julio Cesar Fernandez <j.c.jf285@gmail.com>
2026-01-13 12:24:13 -0800

    refactored for code simplicity

--- components/CreatePostModal/CreatePostModal.tsx
+++ components/CreatePostModal/CreatePostModal.tsx
@@ -23,37 +23,21 @@ export default function CreatePostModal({ onClose }: CreatePostModalProps) {
     //  TODO
   }
 
-  if (user === undefined) {
-    return (
-      <div
-        className={styles.backdrop}
-        onClick={user === undefined ? undefined : onClose}  // prevent closing modal while it is still loading
-      >
+  return (
+    <div
+      className={styles.backdrop}
+      onClick={user === undefined ? undefined : onClose}  // prevent closing modal while it is still loading
+    >
+      {user === undefined ? (
         <div className={styles.modalForm}>
           <h3 className={styles.loadingUser}>Getting things ready<span className={styles.dots}></span></h3>
         </div>
-      </div>
-    );
-  }
-
-  if (user) {
-    return (
-      <div
-        className={styles.backdrop}
-        onClick={user === undefined ? undefined : onClose}  // prevent closing modal while it is still loading
-      >
+      ) : user ? (
         <form className={styles.modalForm} onSubmit={handleSubmit}>
           <h2 className={styles.formHeader}>Create Post</h2>
           <button className={styles.primaryBtn} type="submit">Upload</button>
         </form>
-      </div>
-    );
-  } else {
-    return (
-      <div
-        className={styles.backdrop}
-        onClick={user === undefined ? undefined : onClose}  // prevent closing modal while it is still loading
-      >
+      ) : (
         <div className={styles.modalForm}>
           <h2 className={styles.formHeader}>You Must Be Signed In To Upload Posts</h2>
           <h3 className={styles.formSubHeader}>Already have an account?</h3>
@@ -61,7 +45,7 @@ export default function CreatePostModal({ onClose }: CreatePostModalProps) {
           <h3 className={styles.formSubHeader}>New?</h3>
           <button className={styles.secondaryBtn} type="button" onClick={async () => router.push("/signUp")}> Create Account </button>
         </div>
-      </div>
-    );
-  }
+      )}
+    </div>
+  );
 }
