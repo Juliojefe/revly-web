03fe53c189ed59a3c66a579d56f123e625114bdb
Julio Cesar Fernandez <j.c.jf285@gmail.com>
2026-01-13 12:14:51 -0800

    user present vs guest modal behavior

--- components/CreatePostModal/CreatePostModal.tsx
+++ components/CreatePostModal/CreatePostModal.tsx
@@ -1,10 +1,18 @@
+import { ReactNode, useEffect, useState } from "react";
 import styles from "./CreatePostModal.module.css";
+import axios from 'axios';
+import { useRouter } from "next/navigation";
+import { useUser } from "../../app/providers/UserProvider";
+
 
 type CreatePostModalProps = {
   onClose: () => void;
 };
 
 export default function CreatePostModal({ onClose }: CreatePostModalProps) {
+  const { user } = useUser();
+  const router = useRouter();
+  const [checking, setChecking] = useState(true);
 
   function doNothing() {
     return;
@@ -15,12 +23,45 @@ export default function CreatePostModal({ onClose }: CreatePostModalProps) {
     //  TODO
   }
 
-  return (
-    <div className={styles.backdrop} onClick={onClose}>
-      <form className={styles.modalForm} onSubmit={handleSubmit}>
-        <h2 className={styles.formHeader}>Create Post</h2>
-        <button className={styles.primaryBtn} type="submit">Upload</button>
-      </form>
-    </div>
-  );
+  if (user === undefined) {
+    return (
+      <div
+        className={styles.backdrop}
+        onClick={user === undefined ? undefined : onClose}  // prevent closing modal while it is still loading
+      >
+        <div className={styles.modalForm}>
+          <h3 className={styles.loadingUser}>Getting things ready<span className={styles.dots}></span></h3>
+        </div>
+      </div>
+    );
+  }
+
+  if (user) {
+    return (
+      <div
+        className={styles.backdrop}
+        onClick={user === undefined ? undefined : onClose}  // prevent closing modal while it is still loading
+      >
+        <form className={styles.modalForm} onSubmit={handleSubmit}>
+          <h2 className={styles.formHeader}>Create Post</h2>
+          <button className={styles.primaryBtn} type="submit">Upload</button>
+        </form>
+      </div>
+    );
+  } else {
+    return (
+      <div
+        className={styles.backdrop}
+        onClick={user === undefined ? undefined : onClose}  // prevent closing modal while it is still loading
+      >
+        <div className={styles.modalForm}>
+          <h2 className={styles.formHeader}>You Must Be Signed In To Upload Posts</h2>
+          <h3 className={styles.formSubHeader}>Already have an account?</h3>
+          <button className={styles.primaryBtn} type="button" onClick={async () => router.push("/login")}> Login </button>
+          <h3 className={styles.formSubHeader}>New?</h3>
+          <button className={styles.secondaryBtn} type="button" onClick={async () => router.push("/signUp")}> Create Account </button>
+        </div>
+      </div>
+    );
+  }
 }
