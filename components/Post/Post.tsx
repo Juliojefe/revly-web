bc9063fc6fb862045af092518c756c1be190b94c
Julio Cesar Fernandez <j.c.jf285@gmail.com>
2026-01-17 10:38:01 -0800

    post compontent refactor

--- components/Post/Post.tsx
+++ components/Post/Post.tsx
@@ -14,6 +14,8 @@ interface PostProps {
 export default function Post({ postData = null }: PostProps) {
   const router = useRouter();
   const [currImageIndex, setCurrImageIndex] = useState(0);
+  const { user } = useUser();
+  const hasImage = (postData?.imageUrls?.length ?? 0) > 0;  //  true if one image or more false otherwise
 
   if (!postData) {
     return null;
@@ -49,58 +51,30 @@ export default function Post({ postData = null }: PostProps) {
     return;
   }
 
-  if (postData.imageUrls.length === 0) {
-    return (
-      <div className={styles.postContainer}>
-        {/* header section */}
-        <div
-          className={styles.headerSection}
-          onClick={() => handleGoToProfile(postData.authorId)}
-        >
-          <img
-            className={styles.profilePic}
-            src={postData.createdByProfilePicUrl}
-            alt="profile picture"
-          />
-          <p className={styles.userName}>{postData.createdBy}</p>
-        </div>
-        {/* description section */}
-        <p className={styles.postDescription}>{postData.description || ""}</p>
-        {/* like count */}
-        <p className={styles.likeCount}>{postData.likeCount || 0} likes</p>
-        {/* like comment save icons */}
-        <div className={styles.actionIcons}>
-          <FaRegHeart
-            className={styles.icon}
-            onClick={() => handleLike()}
-          />
-          <FaRegComment
-            className={styles.icon}
-            onClick={() => handleComments()}
-          />
-          <FaRegBookmark
-            className={styles.icon}
-            onClick={() => handleSave()}
-          />
-        </div>
+  /**
+   * Next, I will simplify the sending logic below, as there is currently a lot of duplicate and repeated code.
+   * I will also handle the case where `authorId` is null.
+   * 
+   * Note: `authorId` may be null if a user has deleted their account. In that scenario,
+   * the `user_id` associated with the post is set to null via database cascade rules.
+   */
+
+  return (
+    <div className={styles.postContainer}>
+      {/* header section */}
+      <div
+        className={styles.headerSection}
+        onClick={() => handleGoToProfile(postData.authorId)}
+      >
+        <img
+          className={styles.profilePic}
+          src={postData.createdByProfilePicUrl}
+          alt="profile picture"
+        />
+        <p className={styles.userName}>{postData.createdBy}</p>
       </div>
-    );
-  } else {
-    return (
-      <div className={styles.postContainer}>
-        {/* header section */}
-        <div
-          className={styles.headerSection}
-          onClick={() => handleGoToProfile(postData.authorId)}
-        >
-          <img
-            className={styles.profilePic}
-            src={postData.createdByProfilePicUrl}
-            alt="profile picture"
-          />
-          <p className={styles.userName}>{postData.createdBy}</p>
-        </div>
-        {/* image section */}
+      {/* Image Section */}
+      {hasImage ? (
         <div className={styles.imageSection}>
           {postData.imageUrls?.[0] && (
             <img
@@ -120,26 +94,121 @@ export default function Post({ postData = null }: PostProps) {
             </button>
           )}
         </div>
-        {/* like count */}
-        <p className={styles.likeCount}>{postData.likeCount || 0} likes</p>
-        {/* like comment save icons */}
-        <div className={styles.actionIcons}>
-          <FaRegHeart
-            className={styles.icon}
-            onClick={() => handleLike()}
-          />
-          <FaRegComment
-            className={styles.icon}
-            onClick={() => handleComments()}
-          />
-          <FaRegBookmark
-            className={styles.icon}
-            onClick={() => handleSave()}
-          />
-        </div>
-        {/* description section */}
-        <p className={styles.postDescription}>{postData.description || ""}</p>
+      ) : null}
+      {/* description section */}
+      <p className={styles.postDescription}>{postData.description || ""}</p>
+      {/* like count */}
+      <p className={styles.likeCount}>{postData.likeCount || 0} likes</p>
+      {/* like comment save icons */}
+      <div className={styles.actionIcons}>
+        <FaRegHeart
+          className={styles.icon}
+          onClick={() => handleLike()}
+        />
+        <FaRegComment
+          className={styles.icon}
+          onClick={() => handleComments()}
+        />
+        <FaRegBookmark
+          className={styles.icon}
+          onClick={() => handleSave()}
+        />
       </div>
-    );
-  }
+    </div>
+
+  )
+
+  //   if (postData.imageUrls.length === 0) {
+  //     return (
+  //       <div className={styles.postContainer}>
+  //         {/* header section */}
+  //         <div
+  //           className={styles.headerSection}
+  //           onClick={() => handleGoToProfile(postData.authorId)}
+  //         >
+  //           <img
+  //             className={styles.profilePic}
+  //             src={postData.createdByProfilePicUrl}
+  //             alt="profile picture"
+  //           />
+  //           <p className={styles.userName}>{postData.createdBy}</p>
+  //         </div>
+  //         {/* description section */}
+  //         <p className={styles.postDescription}>{postData.description || ""}</p>
+  //         {/* like count */}
+  //         <p className={styles.likeCount}>{postData.likeCount || 0} likes</p>
+  //         {/* like comment save icons */}
+  //         <div className={styles.actionIcons}>
+  //           <FaRegHeart
+  //             className={styles.icon}
+  //             onClick={() => handleLike()}
+  //           />
+  //           <FaRegComment
+  //             className={styles.icon}
+  //             onClick={() => handleComments()}
+  //           />
+  //           <FaRegBookmark
+  //             className={styles.icon}
+  //             onClick={() => handleSave()}
+  //           />
+  //         </div>
+  //       </div>
+  //     );
+  //   } else {
+  //     return (
+  //       <div className={styles.postContainer}>
+  //         {/* header section */}
+  //         <div
+  //           className={styles.headerSection}
+  //           onClick={() => handleGoToProfile(postData.authorId)}
+  //         >
+  //           <img
+  //             className={styles.profilePic}
+  //             src={postData.createdByProfilePicUrl}
+  //             alt="profile picture"
+  //           />
+  //           <p className={styles.userName}>{postData.createdBy}</p>
+  //         </div>
+  //         {/* image section */}
+  //         <div className={styles.imageSection}>
+  //           {postData.imageUrls?.[0] && (
+  //             <img
+  //               className={styles.postImage}
+  //               src={postData.imageUrls[currImageIndex]}
+  //               alt="post image"
+  //             />
+  //           )}
+  //           {postData.imageUrls.length > 1 && currImageIndex > 0 && (
+  //             <button className={styles.prevImageButton} onClick={handleShowPrevImage}>
+  //               <FaChevronLeft />
+  //             </button>
+  //           )}
+  //           {postData.imageUrls.length > 1 && currImageIndex < postData.imageUrls.length - 1 && (
+  //             <button className={styles.nextImageButton} onClick={handleShowNextImage}>
+  //               <FaChevronRight />
+  //             </button>
+  //           )}
+  //         </div>
+  //         {/* like count */}
+  //         <p className={styles.likeCount}>{postData.likeCount || 0} likes</p>
+  //         {/* like comment save icons */}
+  //         <div className={styles.actionIcons}>
+  //           <FaRegHeart
+  //             className={styles.icon}
+  //             onClick={() => handleLike()}
+  //           />
+  //           <FaRegComment
+  //             className={styles.icon}
+  //             onClick={() => handleComments()}
+  //           />
+  //           <FaRegBookmark
+  //             className={styles.icon}
+  //             onClick={() => handleSave()}
+  //           />
+  //         </div>
+  //         {/* description section */}
+  //         <p className={styles.postDescription}>{postData.description || ""}</p>
+  //       </div>
+  //     );
+  //   }
 }
